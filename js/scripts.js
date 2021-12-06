d92b2d993366e97e2993853a69daef9d62ab7301
Sabrina Grivas <sgrivas@ufl.edu>
2021-12-06 18:44:55 -0500

    added first container styling

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,12 @@ $(document).ready(function(){
         autoplaySpeed: 2000,
     });
   });
-          
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".header").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".header").removeClass("active");
+    }
+});
