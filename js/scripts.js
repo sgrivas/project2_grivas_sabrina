a269f9dd04cd60f7ccb482388563ca8b15c4c5b0
Sabrina Grivas <sgrivas@ufl.edu>
2022-02-11 17:33:33 -0500

    slideshow changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,3 @@
-$(document).ready(function(){
-    $('.featured-slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-    });
-  });
-
 $(window).on("scroll", function() {
     if($(window).scrollTop() > 50) {
         $(".header").addClass("active");
@@ -48,4 +36,33 @@ function openNav() {
 
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
