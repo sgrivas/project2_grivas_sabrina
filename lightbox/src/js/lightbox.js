a9a72385427bec0b2e05343d3afb801d7f9a0982
Sabrina Grivas <sgrivas@ufl.edu>
2022-02-11 10:04:38 -0500

    added new images and responsive changes

--- lightbox/src/js/lightbox.js
+++ lightbox/src/js/lightbox.js
@@ -42,7 +42,7 @@
   // http://lokeshdhakar.com/projects/lightbox2/index.html#options
   Lightbox.defaults = {
     albumLabel: 'Image %1 of %2',
-    alwaysShowNavOnTouchDevices: false,
+    alwaysShowNavOnTouchDevices: true,
     fadeDuration: 600,
     fitImagesInViewport: true,
     imageFadeDuration: 600,
